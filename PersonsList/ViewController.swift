466673dad650c8ea5e782c6f50c6f53ccebc1a4c
shadowSparrow <romanenko.aleksandre@gmail.com>
2022-02-25 13:36:52 +0300

    Initial Commit

---
+++ PersonsList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PersonsList
+//
+//  Created by mac on 25.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
